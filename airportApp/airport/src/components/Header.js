7114f1c6e8586c70725cb48a83d3659128799cf4
Ivan <ivanbazhko3@gmail.com>
2023-10-24 22:53:27 +0300

    Started React App Creation. Added Use Case Diagram

---
+++ airportApp/airport/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>
+                Airport
+            </span>
+        </div>
+        <div className='mainlogo'>
+        </div>
+    </header>
+  )
+}
